0b1b410c6d18e530ba3b5285404f408625ce05c6
Jon Pugh <jon@thinkdrop.net>
2016-08-10 16:07:57 -0400

    Removing commented out code.

--- travis/prepare-docker.sh
+++ travis/prepare-docker.sh
@@ -15,21 +15,7 @@ rm /usr/local/bin/docker-compose
 curl -L https://github.com/docker/compose/releases/download/${DOCKER_COMPOSE_VERSION}/docker-compose-`uname -s`-`uname -m` > docker-compose
 chmod +x docker-compose
 mv docker-compose /usr/local/bin
+
+# Output versions
 docker --version
 docker-compose --version
-
-echo "Adding user travis to docker group..."
-usermod -aG docker travis
-
-# Prepare local docker container
-# @TODO: This is really a pain. Travis is always 1000, standard for ubuntu.
-#   I can build a local container locally with the Dockerfile-local, but it's not working on travis.
-#   So instead, I created a docker aegir/hostmaster:local image on docker hub.
-#USER_UID=`id -u travis`
-#echo "Preparing local aegir container for UID $USER_UID"
-
-# Get Dockerfile
-# wget https://raw.githubusercontent.com/aegir-project/dockerfiles/master/Dockerfile-local
-
-# Run Docker build
-# docker build --build-arg AEGIR_UID=1000 -t aegir/hostmaster:local -f Dockerfile-local .
